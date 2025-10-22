581707467a49b9d05cd5472606be57f175e5d832
mcd25-arch <mcd25@njit.edu>
2025-10-21 23:03:36 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.markslab4;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void displayImage(View view){
+        EditText et;
+        String img;
+
+        et = (EditText) findViewById(R.id.edit_infile);
+        img = et.getText().toString();
+        Uri web = Uri.parse(img);
+        SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.image_area);
+        draweeView.setImageURI(web);
+
+    }
+}
---
+++ MyApplication.java
@@ -0,0 +1,13 @@
+package com.example.markslab4;
+import android.app.Application;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
+
